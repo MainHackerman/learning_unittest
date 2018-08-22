c38bee09666d30ef96e6cdc248660a523dc236cc
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-08-22 11:40:51 +0200

    added unittest for username func

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ class TestMathFuncs(unittest.TestCase):
     def test_fibo(self):
         self.assertEqual(script.fibo(1), 1)
 
+class TestStrFuncs(unittest.TestCase):
+    def test_username(self):
+        self.assertEqual(script.username('hovno@gmail.com'), 'hovno')
+
 if __name__ == '__main__':
     unittest.main()
